3440e5d4a3e5ec6ae04fe67cb74e990ac049e4ae
mirafan <84376469+affanmir19@users.noreply.github.com>
2022-08-01 20:17:11 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,13 @@
-//swapping two numbers
+//swapping two numbers and taking inputs from the user
 #include <stdio.h>
 int main()
 {
-    int x=4,y=5,temp;
-    temp=x;
-    x=y;
-    y=temp;
-    printf("%d",y);
+    int a,b,temp;
+    printf("Enter the value of a and b :");
+    scanf("%d%d",&a,&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("The value of a is %d",a);
     return 0;
 }
