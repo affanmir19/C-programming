599adb17e4ac3ffd5e39e3de0810d6385bc466e1
MIR AFAN <84376469+affanmir19@users.noreply.github.com>
2022-11-08 09:40:30 +0530

    Update and rename recur_fact.c to recursion_fact.c

--- recursion_fact.c
+++ recursion_fact.c
@@ -1,10 +1,11 @@
 #include<stdio.h>  
 int factorial(int n)  
+//recursive function
 {  
-  if (n == 0)  
+  if (n == 0)                    //base case 
     return 1;  
   else  
-    return(n * factorial(n-1));  
+    return(n * factorial(n-1));  //recusrsive procedure  
 }  
    
 void main()  
