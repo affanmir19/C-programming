7d6f7d6bcb9f5d4c12c5b07aaf7d47a918b77e93
MIR AFAN <84376469+affanmir19@users.noreply.github.com>
2022-11-08 12:37:53 +0530

    Update recursion_sum.c

--- recursion_sum.c
+++ recursion_sum.c
@@ -1,7 +1,8 @@
 //finding sum of natural numbers using recursion
+#include <stdio.h>
 int sum(int n){
-    if(n==1)
-      return 1;
+    if(n==0)
+      return 0;
     else
       return sum(n-1)+n;  
 }
