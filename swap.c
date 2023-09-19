919f6c655914513a09248348fd9bc985dfc71471
MIR AFAN <84376469+affanmir19@users.noreply.github.com>
2023-09-19 09:14:43 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,13 +1,18 @@
-//swapping two numbers and taking inputs from the user
+// swapping a number
 #include <stdio.h>
 int main()
 {
-    int a,b,temp;
+    int a, b, temp;
     printf("Enter the value of a and b :");
-    scanf("%d%d",&a,&b);
-    temp=a;
-    a=b;
-    b=temp;
-    printf("The value of a is %d",a);
-    return 0;
+    scanf("%d%d", &a, &b);
+    
+    printf("\nValue of a before swapping = %d\n", a);
+    printf("Value of b before swapping = %d\n", b);
+    
+    temp = a;    /*Logic*/
+    a = b;
+    b = temp;
+    
+    printf("\nValue of a after swapping= %d\n", a);
+    printf("Value of b after swapping= %d\n", b);
 }
